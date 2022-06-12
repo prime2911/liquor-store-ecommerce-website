078869fbbc231a36fb753613a2babd937d9a780a
prime2911 <90593390+prime2911@users.noreply.github.com>
2022-06-12 19:26:27 +0700

    Made username required

--- shop/admin/forms.py
+++ shop/admin/forms.py
@@ -3,7 +3,7 @@ from wtforms import Form, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
     name = StringField("Name", [validators.Length(min=4, max=200)])
-    username = StringField("Username", [validators.Length(min=4, max=200)])
+    username = StringField("Username", [validators.Length(min=4, max=200), validators.DataRequired()])
     email = StringField(
         "Email Address", [validators.Length(min=6, max=200), validators.Email()])
     password = PasswordField("Password", [
