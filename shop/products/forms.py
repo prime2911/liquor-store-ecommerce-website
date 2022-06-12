93c43cb84a93b782b36889e94a6f1321db26d8cf
prime2911 <90593390+prime2911@users.noreply.github.com>
2022-06-12 19:26:58 +0700

    Added fields' length limit to match database's

--- shop/products/forms.py
+++ shop/products/forms.py
@@ -2,8 +2,8 @@ from flask_wtf.file import FileAllowed, FileField
 from wtforms import Form, IntegerField, DecimalField, StringField, TextAreaField, validators
 
 class ProductForm(Form):
-    name = StringField("Name", [validators.DataRequired()])
-    origins = StringField("Country of Origins", [validators.DataRequired()])
+    name = StringField("Name", [validators.Length(max=200), validators.DataRequired()])
+    origins = StringField("Country of Origins", [validators.Length(max=100), validators.DataRequired()])
     price = DecimalField("Price", [validators.DataRequired()])
     discount = IntegerField("Discount", default=0)
     stock = IntegerField("Stock", [validators.DataRequired()])
